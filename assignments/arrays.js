231668908c5772747accac06bba12d19483a1afb
Ray Aguilar <ray.aguilar15@gmail.com>
2019-06-29 08:50:33 -0500

    stuck on arrays, will study and come back to this later during the break

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -72,14 +72,22 @@ console.log(`${lastCar.car_make} ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
+let carModels = inventory[inventory.sort()];
 
-console.log();
+console.log(inventory);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
-console.log();
+
+for(let i = 0; i < 0; i++) {
+    let obj = {};
+    obj.carName = inventory[i].car_model;
+    carYears.push(obj);
+    console.log(carYears);
+}
+
+console.log(carYears.sort());
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
