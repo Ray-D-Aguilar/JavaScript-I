e40f296ec6c9f20050e893eb984aabdf93252ac7
Ray Aguilar <ray.aguilar15@gmail.com>
2019-06-29 08:51:06 -0500

    Stuck on arrays will come back to this later

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -72,7 +72,9 @@ console.log(`${lastCar.car_make} ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = inventory[inventory.sort()];
+let carModels = []
+
+for
 
 console.log(inventory);
 
@@ -80,14 +82,8 @@ console.log(inventory);
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
 let carYears = [];
 
-for(let i = 0; i < 0; i++) {
-    let obj = {};
-    obj.carName = inventory[i].car_model;
-    carYears.push(obj);
-    console.log(carYears);
-}
 
-console.log(carYears.sort());
+console.log();
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
@@ -97,6 +93,7 @@ console.log();
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 let BMWAndAudi = [];
+
 console.log();
 
 
